36a46b8190f6e0ee2d58952392e2611d781c01a7
Joeri Kassenaar <joeri@Joeris-MacBook-Air.local>
2024-11-26 18:00:15 +0100

    Documentation GoogleId

---
+++ src/components/views/documentation/tracker/TrackerGoogle.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="scroll-content">
+        UTM codes<br><br>
+
+        <step-wrap step="1" label="Source">
+           _gl
+        </step-wrap>
+
+        <step-wrap step="2" label="Medium">
+            gclid
+        </step-wrap>
+
+        <step-wrap step="3" label="Name">
+            gclsrc
+        </step-wrap>
+
+    </div>
+</template>
